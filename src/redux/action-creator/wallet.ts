2a27532058e7f8dfd26af5728d18b09e02e68e1e
Dmytro-Kyparenko <dmytro_kyparenko@adoriasoft.com>
2021-07-23 12:54:27 +0300

    Added wallet to redux

---
+++ src/redux/action-creator/wallet.ts
@@ -0,0 +1,30 @@
+import {Dispatch} from "redux";
+import {WalletAction, WalletActionTypes} from "../../types/wallet";
+import {chainInfo} from "../../config";
+import {getKeplr} from "../../cosmos";
+
+export const connectWallet = () => {
+    return async (dispatch: Dispatch<WalletAction>) => {
+        try {
+            dispatch({type: WalletActionTypes.WALLET_CONNECT})
+
+            const keplr = await getKeplr();
+            if (!keplr) {
+                throw new Error('Keplr extension not found');
+            }
+
+            await keplr.experimentalSuggestChain(chainInfo);
+            await keplr.enable(chainInfo.chainId);
+
+
+            dispatch({type: WalletActionTypes.WALLET_SUCCESS, payload: keplr})
+        } catch (e) {
+            dispatch({type: WalletActionTypes.WALLET_ERROR, payload: e.message || 'error'});
+        }
+    }
+}
+
+export const disconnectWallet = () => {
+    return {type: WalletActionTypes.WALLET_DISCONNECT}
+}
+
--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,8 +1,10 @@
 import {combineReducers} from "redux";
 import {proposalReducer} from "./proposal";
+import {walletReducer} from "./wallet";
 
 export const rootReducer = combineReducers({
     proposal: proposalReducer,
+    wallet: walletReducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>;
