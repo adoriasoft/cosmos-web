e765bcf0e7717777cdc9c07b74e1ce9092e6c079
Dmytro-Kyparenko <dmytro_kyparenko@adoriasoft.com>
2021-08-04 14:58:58 +0300

    Fix compiled

--- src/components/SubmitProposal/ParameterChangeProposal/ParamChange/ChangeForm.tsx
+++ src/components/SubmitProposal/ParameterChangeProposal/ParamChange/ChangeForm.tsx
@@ -12,30 +12,31 @@ const ChangeForm = () => {
     const [value, setValue] = useState("");
 
     return (
-        <div className="admin-page__form">
-            <label className="admin-page__form__label" htmlFor="new-admin">
-                Add deposit
-                <input
-                    value={amount}
-                    onChange={({ target }) => setAmount(target.value)}
-                    className="admin-page__form__address-input"
-                    placeholder="Amount"
-                    type="number"
-                />
-                <input
-                    value={denom}
-                    onChange={({ target }) => setDenom(target.value)}
-                    className="admin-page__form__address-input"
-                    placeholder="Denom"
-                    type="text"
-                />
-            </label>{" "}
-            <button
-                onClick={() => addDeposit(coin(+amount, denom))}
-                className="admin-page__form__save-btn">
-                Add
-            </button>
-        </div>
+        <h1>ChangeForm</h1>
+        // <div className="admin-page__form">
+        //     <label className="admin-page__form__label" htmlFor="new-admin">
+        //         Add deposit
+        //         <input
+        //             value={amount}
+        //             onChange={({ target }) => setAmount(target.value)}
+        //             className="admin-page__form__address-input"
+        //             placeholder="Amount"
+        //             type="number"
+        //         />
+        //         <input
+        //             value={denom}
+        //             onChange={({ target }) => setDenom(target.value)}
+        //             className="admin-page__form__address-input"
+        //             placeholder="Denom"
+        //             type="text"
+        //         />
+        //     </label>{" "}
+        //     <button
+        //         onClick={() => addDeposit(coin(+amount, denom))}
+        //         className="admin-page__form__save-btn">
+        //         Add
+        //     </button>
+        // </div>
     );
 };
 
---
+++ src/components/SubmitProposal/ParameterChangeProposal/ParamChange/ChangeItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChangeItem = () => {
+    return <div>Change Item</div>;
+};
+
+export default ChangeItem;
