97d79f93a7a7fd3d16460e0b2a04129cd5f0a083
Dmytro-Kyparenko <dmytro_kyparenko@adoriasoft.com>
2021-08-05 20:37:52 +0300

    Added submit parameter change proposal

--- src/components/SubmitProposal/ParameterChangeProposal/ParameterChangeProposal.tsx
+++ src/components/SubmitProposal/ParameterChangeProposal/ParameterChangeProposal.tsx
@@ -1,78 +1,50 @@
 import React, { useRef, useState } from "react";
-import { useTypedSelector } from "../../../redux/useTypedSelector";
-import { useDispatch } from "react-redux";
-import { Coin } from "@cosmjs/stargate";
-import DepositForm from "../InitialDeposit/DepositForm";
-import DepositItem from "../InitialDeposit/DepositItem";
 import {
-    saveParameterChangeProposalData,
-    saveParameterChangeProposalDeposits,
-    submitParameterChangeProposal
-} from "../../../redux/action-creator/submitProposal/parameterChangeProposal";
-import { ParamChange } from "../../../cosmos/codec/cosmos/params/v1beta1/params";
+    ParamChange,
+    ParameterChangeProposal as ParameterChangeProposalProc
+} from "../../../cosmos/codec/cosmos/params/v1beta1/params";
 import ChangeForm from "./ParamChange/ChangeForm";
 import ChangeItem from "./ParamChange/ChangeItem";
+import { TBaseSPMsg } from "../../../types/submitProposal";
+import { useDispatch } from "react-redux";
+import { submitProposal } from "../../../redux/action-creator/submitProposal";
 
-const ParameterChangeProposal: React.FC = () => {
-    const {
-        proposal: { description, title, changes },
-        deposits,
-        ...data
-    } = useTypedSelector((state) => state.submitProposal.parameterChangeProposal);
+const ParameterChangeProposal: React.FC<TBaseSPMsg> = ({ title, description, deposit }) => {
+    const [changes, setChanges] = useState<ParamChange[]>([]);
     const dispatch = useDispatch();
-    const setTitle = (data: string) => {
-        dispatch(saveParameterChangeProposalData({ title: data, description, changes }));
-    };
-    const setDescription = (data: string) => {
-        dispatch(saveParameterChangeProposalData({ title, description: data, changes }));
-    };
-    const setChanges = (data: ParamChange[]) => {
-        dispatch(saveParameterChangeProposalData({ title, description, changes: data }));
-    };
-    const setDeposits = (data: Coin[]) => {
-        dispatch(saveParameterChangeProposalDeposits(data));
+    const submitParameterChangeProposal = () => {
+        dispatch(
+            submitProposal(
+                {
+                    typeUrl: "/cosmos.params.v1beta1.ParameterChangeProposal",
+                    value: ParameterChangeProposalProc.encode({
+                        title,
+                        description,
+                        changes
+                    }).finish()
+                },
+                deposit
+            )
+        );
     };
 
     return (
         <div>
-            {data.error}
-            {data.broadcastResponse?.rawLog}
             <div>
-                <input
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    type={"text"}
-                    placeholder={"Title"}
-                />
-                <input
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    type={"text"}
-                    placeholder={"Description"}
-                />
-                <button onClick={() => dispatch(submitParameterChangeProposal())}>Send</button>
+                <ChangeForm addChange={(c) => setChanges([...changes, c])} />
+                {changes.map((c, i) => (
+                    <ChangeItem
+                        key={i}
+                        change={c}
+                        deleteChange={() =>
+                            setChanges([...changes.slice(0, i), ...changes.slice(i + 1)])
+                        }
+                    />
+                ))}
             </div>
-            <DepositForm addDeposit={(d) => setDeposits([...deposits, d])} />
-            {deposits.map((d, i) => (
-                <DepositItem
-                    key={i}
-                    deposit={d}
-                    deleteDeposit={() =>
-                        setDeposits([...deposits.slice(0, i), ...deposits.slice(i + 1)])
-                    }
-                />
-            ))}
-
-            <ChangeForm addChange={(c) => setChanges([...changes, c])} />
-            {changes.map((c, i) => (
-                <ChangeItem
-                    key={i}
-                    change={c}
-                    deleteChange={() =>
-                        setChanges([...changes.slice(0, i), ...changes.slice(i + 1)])
-                    }
-                />
-            ))}
+            <div>
+                <button onClick={submitParameterChangeProposal}>Submit</button>
+            </div>
         </div>
     );
 };
